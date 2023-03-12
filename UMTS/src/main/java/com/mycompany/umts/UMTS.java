3a84eaf203dbfc860f775dbcf6601bb80579a647
Mel1o <101039200+Mel1o@users.noreply.github.com>
2023-03-12 21:38:08 +0800

    Add files via upload

---
+++ UMTS/src/main/java/com/mycompany/umts/UMTS.java
@@ -0,0 +1,22 @@
+package com.mycompany.umts;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.ImageIcon;
+
+public class UMTS {
+    public static void main(String[] args) {
+    //For the Canvas
+    int wid =1920, hei=1080;
+    
+    RectangleCanvas rc=new RectangleCanvas(wid,hei,Color.white);
+    JFrame frame= rc.getFrame();
+    Container cp=rc.getContentPane();
+    frame.add(rc); 
+    frame.setTitle("EasyWorkload");
+    cp.add(rc,BorderLayout.CENTER);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+    rc.setUpTimer();
+}
+}
